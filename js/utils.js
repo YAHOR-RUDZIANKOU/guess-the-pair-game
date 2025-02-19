08eebde8448b329575adb86d4d8334a8ee6ab234
Egor Rudenkov <rudenkov.egor03@mail.ru>
2025-02-19 20:42:55 +0300

    feat: added fireconfetti function that will be triggered at the end of the game

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,3 @@
-
-
 export const shuffleArray = (arr) => {
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -26,3 +24,23 @@ export const createIconsArray = (dif) => {
   }
 };
 
+// fireConfetti - логика для confetti
+export const fireConfetti = () => {
+  let duration = 15 * 1000;
+  let animationEnd = Date.now() + duration;
+  let defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+  
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  
+  let interval = setInterval(function() {
+    let timeLeft = animationEnd - Date.now();
+    if (timeLeft <= 0) {
+      clearInterval(interval);
+    }
+    let particleCount = 50 * (timeLeft / duration);
+    confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
+    confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
+  }, 250);
+};
